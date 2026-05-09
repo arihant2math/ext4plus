4c19d08236bd87a1a9d01e88f2044d298976ea64
Ashwin Naren <arihant2math@gmail.com>
2026-05-08 20:18:57 -0700

    update comment

--- src/lib.rs
+++ src/lib.rs
@@ -1007,7 +1007,7 @@ impl Ext4 {
         &self,
         options: InodeCreationOptions,
     ) -> Result<Inode, Ext4Error> {
-        // TODO: for the purposes of fsck, it is proper to write inode data, then mark as used
+        // TODO: for the purposes of fscking during recovery, it is proper to write inode data, then mark as used
         let inode_index = self.alloc_inode(options.file_type).await?;
         Inode::create(inode_index, options, self).await
     }
